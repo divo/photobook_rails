4d90d154388d8ce986f36d6310677fef757a7121
divo <divoapple@gmail.com>
2022-11-11 17:12:03 +0000

    PhotoAlbum basic idea for styling

--- app/javascript/photo-canvas/page.js
+++ app/javascript/photo-canvas/page.js
@@ -3,34 +3,31 @@ import CanvasSketch from 'canvas-sketch';
 import load from 'load-asset';
 import fs from 'graceful-fs';
 
-const canvas = Canvas.createCanvas();
-
-// How to share this across multiple images? Just pass it through
-let img;
 const safe_area = 15;
 
-const settings = {
-  canvas,
-  dimensions: [ 210, 210],
-  units: 'mm'
-};
-
-const sketch = ({width, height, canvas}) => {
-  return ({ context, width, height }) => {
-    //context.fillStyle = 'white';
-    //context.fillRect(0, 0, width, height);
-    context.drawImage(img, safe_area, safe_area, width - (safe_area * 2), height - (safe_area * 2));
+const sketch = ({width, height, canvas, data}) => {
+  return ({ context, width, height, data }) => {
+    context.drawImage(data, safe_area, safe_area, width - (safe_area * 2), height - (safe_area * 2));
   };
 };
 
 const start = async function (parent, img_url) {
-  img = await load(img_url);
+  const canvas = Canvas.createCanvas();
+  const settings = {
+    canvas,
+    dimensions: [210, 210],
+    units: 'mm',
+  };
+
+  let img = await load(img_url);
   settings.parent = parent;
-  CanvasSketch.canvasSketch(sketch, settings)
+  settings.data = img;
+  CanvasSketch.canvasSketch(sketch, settings);
 };
 
 document.addEventListener("turbo:load", function() {
   document.querySelectorAll('.photo-page').forEach(function(canvas_tag) {
+    //TODO: Disable keyboard shortcuts
     const img_url = canvas_tag.dataset['url'];
     start(canvas_tag, img_url);
   });
