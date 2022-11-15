24ef42141e310d4a5990d7e7af7a1ff512fff6fb
divo <divoapple@gmail.com>
2022-11-15 11:15:36 +0000

    Added dropzone and its css
    importmap is only for JS. I'm not sure how bootstrap works but it must
    be non-standard. For "normal" projects css needs to be fetched outside
    of importmap.
    
    This whole importmap thing seems a little half baked.

--- config/importmap.rb
+++ config/importmap.rb
@@ -26,3 +26,5 @@ pin "stream", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/brow
 pin "util", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/util.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.2/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
+pin "dropzone", to: "https://ga.jspm.io/npm:dropzone@6.0.0-beta.2/dist/dropzone.mjs"
+pin "just-extend", to: "https://ga.jspm.io/npm:just-extend@5.1.1/index.esm.js"
