52d33e08bb0088e090a4a3f0c2edacb73bfb523d
divo <divoapple@gmail.com>
2022-11-16 16:27:34 +0000

    Scale the image correctly. This approach is not ideal
    First, I think manually specifying the dimensions is bugged. So changed to use a preset
    The canvas is crap at scaling images. If we load the full thing and let the canvas scale it
    the result looks poor.
    If we scale the image down to fit the canvas the result looks low resolution

--- app/javascript/photo-canvas/page.js
+++ app/javascript/photo-canvas/page.js
@@ -1,22 +1,28 @@
 import Canvas from 'canvas';
 import CanvasSketch from 'canvas-sketch';
 import load from 'load-asset';
-import fs from 'graceful-fs';
 
 const safe_area = 15;
 
 const sketch = ({width, height, canvas, data}) => {
   return ({ context, width, height, data }) => {
-    context.drawImage(data, safe_area, safe_area, width - (safe_area * 2), height - (safe_area * 2));
+    context.drawImage(data, 0, 0, width, height);
   };
 };
+
+const scale = (image, canvas) => {
+  // TODO: Get the larger dimension and scale the image by it on the correct
+  // dimension in order to handle portrait images
+  return image.width / canvas.width
+}
 
 const start = async function (parent, img_url) {
   const canvas = Canvas.createCanvas();
   const settings = {
     canvas,
-    dimensions: [210, 210],
-    units: 'mm',
+    dimensions: '8r',
+    pixelsPerInch: 300,
+    orientation: 'landscape',
   };
 
   let img = await load(img_url);
