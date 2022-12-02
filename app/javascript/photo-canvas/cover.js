39ab9ff26c90330c286c0502620abfb53e061933
divo <divoapple@gmail.com>
2022-12-02 12:10:12 +0000

    Render title cover

--- app/javascript/photo-canvas/cover.js
+++ app/javascript/photo-canvas/cover.js
@@ -6,6 +6,8 @@ const safe_area = 15; // mm!
 
 const sketch = ({width, height, canvas, data}) => {
   return ({ context, width, height, data, canvas }) => {
+    const img = data['img'];
+    const name = data['name'];
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
@@ -13,15 +15,22 @@ const sketch = ({width, height, canvas, data}) => {
     let y = 0;
     let x = 0;
 
-    if (is_landscape(data)) {
-      scale = width / data.width;
-      y = 0;
-    } else { // TODO: How to layout landscape covers? Easiest way is to simply not allow it
-      scale = height / data.height;
-      x = 0;
+    if (is_landscape(img)) {
+      scale = width / img.width;
+    } else {
+      scale = height / img.height; // TODO: How to layout landscape covers? Easiest way is to simply not allow it
     }
 
-    context.drawImage(data, x, y, (data.width * scale), (data.height * scale));
+    const s_height = img.height * scale;
+
+    context.drawImage(img, x, y, (img.width * scale), s_height);
+
+    const fontSize = 18 * scale;
+    context.fillStyle = 'black';
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    context.font = `normal ${fontSize}px Helvetica`;
+    context.fillText(name, width / 2, s_height + (( height - s_height) / 2), width);
   };
 };
 
@@ -29,7 +38,10 @@ const is_landscape = (image) => {
   return image.width > image.height;
 };
 
-const start = async function (parent, img_url) {
+const start = async function (parent, dataset) {
+  const img_url = dataset['url'];
+  const name = dataset['name'];
+
   const canvas = Canvas.createCanvas();
   const settings = {
     canvas,
@@ -39,17 +51,17 @@ const start = async function (parent, img_url) {
     units: 'mm',
     hotkeys: false,
     scaleToFitPadding: 0,
+    attributes: { antialias: true }
   };
 
-  let img = await load(img_url); // Can't use on DOM in backend, load manually. Update: Lol, fucking thing uses the window
+  let img = await load(img_url);
   settings.parent = parent;
-  settings.data = img;
+  settings.data = { img: img, name: name };
   CanvasSketch.canvasSketch(sketch, settings);
 };
 
 document.addEventListener("turbo:load", function() {
   document.querySelectorAll('.cover-page').forEach(function(canvas_tag) {
-    const img_url = canvas_tag.dataset['url'];
-    start(canvas_tag, img_url);
+    start(canvas_tag, canvas_tag.dataset);
   });
 });
