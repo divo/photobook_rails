e6bbf36de653e6f081c13c4eab39db7124f10506
divo <divoapple@gmail.com>
2023-01-05 16:28:41 +0000

    Static page controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
   resources :photos
   resources :photo_albums
   get 'photo_albums/:id/print', to: 'photo_albums#print', as: 'print_photo_album'
