d2d669574a9c20ce78eab800db22ee6e73c39edb
divo <divoapple@gmail.com>
2023-02-09 15:30:23 +0000

    Fix RenderJob timeout
    Allow to run for 5 minutes. This is ok now. If I get any decent traffic I'll need to rework it

--- lib/render_client.rb
+++ lib/render_client.rb
@@ -15,7 +15,15 @@ class RenderClient
   def render_album(job_id)
     # TODO: Create a record of the render request, an 'Order'
     options = @photo_album.merge(job_id: job_id)
-    options[:size] = [230, 230]
-    self.class.post("/api/render_album", body: options.to_json, headers: { 'Content-Type' => 'application/json' }, debug_output: $stdout )
+    options[:size] = [210, 210]
+    # TODO: Stop abusing long timeout. If I need to serve any level of
+    # concurrency I need to use a queueing system, with something to
+    # track and check job progress
+    self.class.post("/api/render_album",
+                    body: options.to_json,
+                    headers: { 'Content-Type' => 'application/json' },
+                    timeout: 60 * 60, # Allow render 5 minutes to complete
+                    debug_output: $stdout )
+    Rails.logger.info "Rendered album #{job_id}"
   end
 end
