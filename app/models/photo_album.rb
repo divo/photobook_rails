1978acbb9f23af9510203deab247a0e861d9da28
divo <divoapple@gmail.com>
2023-02-09 14:08:19 +0000

    Basic form validation on for PhotoAlbum

--- app/models/photo_album.rb
+++ app/models/photo_album.rb
@@ -1,4 +1,24 @@
+class AlbumValidator < ActiveModel::Validator
+  def validate(record)
+    if !record.name.present?
+      record.errors.add :name, "The album must have a name"
+    end
+
+    if record.images.length < record.class.min_images
+      record.errors.add :images, "Album must have at least #{record.class.min_images} images"
+    elsif record.images.length > 200
+      record.errors.add :images, "Album must have at most 200 images"
+    end
+  end
+end
+
 class PhotoAlbum < ApplicationRecord
   include Presentable
+  include ActiveModel::Validations
   has_many_attached :images
+  validates_with ::AlbumValidator
+
+  def self.min_images
+    5
+  end
 end
