b7e673fe7ba5e6b256b1c3a357f3e4825697c5d8
divo <divoapple@gmail.com>
2023-02-10 11:00:54 +0000

    Fix for "back" to index rendering multiple canvases

--- app/javascript/photo-canvas/cover.js
+++ app/javascript/photo-canvas/cover.js
@@ -1,8 +1,16 @@
 import render_setup from './render_setup.js'
 import { cover_sketch } from '../sketches/cover-sketch.js';
 
-document.addEventListener("turbo:load", function() {
+document.addEventListener("turbo:load", function(e) {
   document.querySelectorAll('.cover-page').forEach(function(canvas_tag) {
     render_setup(cover_sketch, canvas_tag);
+  });
+});
+
+document.addEventListener("turbo:before-cache", function() {
+  // Remove canvas from DOM so turbo doesn't cache it
+  // It doesn't cache the image correctly anyway
+  document.querySelectorAll('.cover-page').forEach(function(canvas_tag) {
+    canvas_tag.innerHTML = '';
   });
 });
