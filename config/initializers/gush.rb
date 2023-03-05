71aaca49324b35fcc7c9031f0ede0beebdf877e9
divo <divoapple@gmail.com>
2023-03-05 17:12:48 +0000

    Gush also requires redis.
    durp

---
+++ config/initializers/gush.rb
@@ -0,0 +1,8 @@
+rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
+redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
+redis_config.merge! redis_config.fetch(Rails.env, {})
+redis_config.symbolize_keys!
+
+Gush.configure do |config|
+  config.redis_url = "redis://#{redis_config[:host]}:#{redis_config[:port]}"
+end
