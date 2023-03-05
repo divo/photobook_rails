19f2e01500c02eb8be1f0b5995c1f1605c92f2c6
divo <divoapple@gmail.com>
2023-03-05 17:27:28 +0000

    Sidekiq requires a redis db

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,9 +4,9 @@ redis_config.merge! redis_config.fetch(Rails.env, {})
 redis_config.symbolize_keys!
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}" }
+  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}" }
+  config.redis = { url: "redis://#{redis_config[:host]}:#{redis_config[:port]}/0" }
 end
