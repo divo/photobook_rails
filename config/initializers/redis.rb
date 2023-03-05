18a00b33981a7097c6ef7a5f8f1f5d0a346b4aa9
divo <divoapple@gmail.com>
2023-03-05 15:37:26 +0000

    Configure RedisClient url
    Why is the so poorly documented

---
+++ config/initializers/redis.rb
@@ -0,0 +1,7 @@
+rails_root = Rails.root || File.dirname(__FILE__) + '/../..'
+rails_env = Rails.env || 'development'
+redis_config = YAML.load_file(rails_root.to_s + '/config/redis.yml')
+redis_config.merge! redis_config.fetch(Rails.env, {})
+redis_config.symbolize_keys!
+
+RedisClient.config(host: redis_config[:host], port: redis_config[:port], db: 0)
