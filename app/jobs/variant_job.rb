800a9671c8370a9a4959aa381e03b6762506b645
divo <divoapple@gmail.com>
2023-03-08 09:13:56 +0000

    Variant needs to be `produced`

--- app/jobs/variant_job.rb
+++ app/jobs/variant_job.rb
@@ -3,7 +3,7 @@ class VariantJob < Gush::Job
     photo_album = PhotoAlbum.find(params[:photo_album_id])
     Rails.logger.info "Starting VariantJob for photo_album #{photo_album.id}"
     photo_album.images.each do |image|
-      image.variant(resize_to_fit: [600, 600])
+      image.variant(resize_to_fit: [600, 600]).processed
     end
   end
 end
