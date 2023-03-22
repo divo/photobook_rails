9f6cfd3dfd29362e68e00523d2ba0e8702ad3a56
divo <divoapple@gmail.com>
2023-03-22 15:35:51 +0000

    Forgot to checkin the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_214938) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_22_152652) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -43,6 +43,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_214938) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "addresses", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name"
+    t.string "line_1"
+    t.string "line_2"
+    t.string "state"
+    t.string "city"
+    t.string "post_code"
+    t.string "country"
+    t.string "email"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "order_estimates", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.decimal "price"
     t.decimal "gelato_price"
@@ -51,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_214938) do
     t.integer "max_delivery_days"
     t.string "currency"
     t.string "shipping_name"
+    t.string "shipping_method_uuid"
     t.uuid "photo_album_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -63,6 +78,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_21_214938) do
     t.integer "state", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.uuid "address_id"
     t.index ["order_estimate_id"], name: "index_orders_on_order_estimate_id"
     t.index ["photo_album_id"], name: "index_orders_on_photo_album_id"
   end
