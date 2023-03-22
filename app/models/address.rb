6b7b9e751a26bfa146eb72763faee623c47c498f
divo <divoapple@gmail.com>
2023-03-22 16:22:11 +0000

    Fixup order serialization

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,17 +1,31 @@
 class Address < ApplicationRecord
   belongs_to :order
 
-  def self.build_from_stripe(session, order)
-    details = session.shipping_details.address
+  def self.build_from_stripe(details, email, order)
+    address = details.address
     Address.new(
       order: order,
       name: details.name,
-      line1: details.line1,
-      line2: details.line2,
-      city: details.city,
-      state: details.state,
-      postal_code: details.postal_code,
-      country: details.country
+      line_1: address.line1,
+      line_2: address.line2,
+      city: address.city,
+      state: address.state,
+      post_code: address.postal_code,
+      country: address.country,
+      email: email
     )
+  end
+
+  def to_gelato
+    {
+      firstName: name,
+      addressLine1: line_1,
+      addressLine2: line_2,
+      city: city,
+      state: state,
+      postCode: post_code,
+      country: country,
+      email: email
+    }
   end
 end
