6115ad8fdd10f98c1b89a51a3203845269816b43
divo <divoapple@gmail.com>
2023-03-24 14:36:27 +0000

    Handle Node app going away, mark the render as failed

--- app/jobs/render_album_job.rb
+++ app/jobs/render_album_job.rb
@@ -6,18 +6,20 @@ class RenderAlbumJob < ApplicationJob
     render_client = RenderClient.new(photo_album, spine_width)
     # Wait for rendering to finish
     logger.info "Begin rendering album #{photo_album['id']}"
-    response = render_client.render_album(self.job_id)
+
+    begin
+      response = render_client.render_album(self.job_id)
+    rescue => e
+      byebug
+      logger.error "Error rendering album #{photo_album['id']}: #{e}"
+      order.render_failed!
+      return
+    end
+
     if response.success?
       logger.info "Finished rendering album #{photo_album['id']}"
       # Store the rendered album in the order
-      blob = ActiveStorage::Blob.create_and_upload!(
-        io: StringIO.new(response.body),
-        filename: "order_#{order.id}.pdf",
-        service_name: :orders_bucket
-      )
-      blob.save!
-      order.rendered_album.attach(blob)
-      order.save!
+      save_rendered_album(order, response)
 
       order.render!
     else
@@ -25,5 +27,18 @@ class RenderAlbumJob < ApplicationJob
       order.render_failed!
     end
     # then download the created album
+  end
+
+  private
+
+  def save_rendered_album(order, response)
+    blob = ActiveStorage::Blob.create_and_upload!(
+      io: StringIO.new(response.body),
+      filename: "order_#{order.id}.pdf",
+      service_name: :orders_bucket
+    )
+    blob.save!
+    order.rendered_album.attach(blob)
+    order.save!
   end
 end
