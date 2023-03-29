07733d81132a8c1904cb91af87dea5ebc2c8f32d
divo <divoapple@gmail.com>
2023-03-29 18:57:06 +0100

    Need to parse the request body -_-

--- app/controllers/gelato_webhooks_controller.rb
+++ app/controllers/gelato_webhooks_controller.rb
@@ -3,7 +3,7 @@ class GelatoWebhooksController < ApplicationController
 
   def create
     Rails.logger.info("Gelato webhook received")
-    payload = request.body.read
+    payload = JSON.parse(request.body.read)
     sig_header = request.headers['GELATO_TOKEN']
     unless sig_header == Rails.application.credentials[:gelato_webhook_secret]
       Rails.logger.error "⚠️  Webhook signature verification failed"
@@ -13,13 +13,16 @@ class GelatoWebhooksController < ApplicationController
     if payload['object'] == 'orderStatus'
       update_order(payload)
     end
+
+    return head :ok
   end
 
   private
 
   def update_order(payload)
     order = Order.find(payload['orderReferenceId'])
-    order.update_gelato_status(payload['fulfillmentStatus'])
+    Rails.logger.error "⚠️  Order #{order.id} not found" if order.nil?
+    order.update_gelato_state(payload['fulfillmentStatus'])
 
     if payload['fulfillmentStatus'] == "shipped"
       item = payload['items'].first
