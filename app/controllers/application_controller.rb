1929ffad70388024a2963be4de07cc21391f4645
divo <divoapple@gmail.com>
2023-06-15 10:03:20 +0100

    Add capability to detect mobile app in views/controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :user_agent
+  before_action :mobile_app?
+
+  def user_agent
+    @user_agent ||= request.user_agent
+  end
+
+  def mobile_app?
+    @mobile_app ||= @user_agent == 'Mementos-iOS'
+  end
 end
