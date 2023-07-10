7598be2efd3d5227002461a2668ae6603fe517c0
divo <divoapple@gmail.com>
2023-07-10 08:57:04 +0000

    User deletion email

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -7,4 +7,13 @@ class RegistrationMailer < ApplicationMailer
          reply_to: Rails.application.credentials.dig(:smtp, :username),
          template_name: 'user_signup')
   end
+
+  def user_delete(user)
+    @user = user
+    mail(to: @user.email,
+         subject: 'You Mementos.ink account was deleted',
+         from: Rails.application.credentials.dig(:smtp, :username),
+         reply_to: Rails.application.credentials.dig(:smtp, :username),
+         template_name: 'user_delete')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_soft_deletion default_scope: true
+  before_soft_delete :validate_deletability
+  after_soft_delete :send_deletion_email
+
   has_many :photo_albums, dependent: :destroy
 
   def currency_iso
     ISO3166::Country[self.country_code.downcase].currency.iso_code
+  end
+
+  def send_deletion_email
+    RegistrationMailer.user_delete(self).deliver_later
   end
 end
