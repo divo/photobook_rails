7e0ffb7dfb85c29d57aa2d3774eef25344589060
divo <divoapple@gmail.com>
2023-08-02 16:57:46 +0100

    Revert puma config to default
    No longer using socker descriptors for everything

--- config/puma.rb
+++ config/puma.rb
@@ -22,24 +22,6 @@ port ENV.fetch("PORT") { 3000 }
 rails_env = ENV.fetch("RAILS_ENV") { "development" }
 environment rails_env
 
-if %w[production staging].member?(rails_env)
-    app_dir = ENV.fetch("APP_DIR") { "photobook_rails" }
-    directory app_dir
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-    shared_dir = ENV.fetch("SHARED_DIR") { "photobook_rails" }
-
-    # Logging
-    stdout_redirect "#{shared_dir}/log/photobook_puma.stdout.log", "#{shared_dir}/log/photobook_puma.stderr.log", true
-
-    pidfile "#{shared_dir}/tmp/pids/puma.pid"
-    state_path "#{shared_dir}/tmp/pids/puma.state"
-
-    # Set up socket location
-    bind "unix://#{shared_dir}/sockets/puma.sock"
-
-    workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-    preload_app!
-
-elsif rails_env == "development"
-    plugin :tmp_restart
-end
+plugin :tmp_restart
