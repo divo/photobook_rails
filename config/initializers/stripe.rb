a2b9354e2e971dd07a428f2a472c0e4dcafc2141
divo <divoapple@gmail.com>
2023-08-03 11:40:46 +0100

    Workaround creds not available during precompile

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,3 @@
-Stripe.api_key = Rails.application.credentials[:stripe][:api_key]
+if ENV['RAILS_ENV'] # Workaround for creds not available in asset precompile
+  Stripe.api_key = Rails.application.credentials[:stripe][:api_key]
+end
