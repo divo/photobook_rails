bcded8b669eadbaaae5d21d6b3d7c54cf2579870
divo <divoapple@gmail.com>
2023-09-05 10:57:54 +0100

    Revert "Add Geocode model & migration"
    This reverts commit 7e130d2ee70a9ee01656022387ab19099a10b42f.

--- app/models/geocode.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-class Geocode < ApplicationRecord
-  belongs_to :photo_album
-  belongs_to :active_storage_blob, class_name: 'ActiveStorage::Blob'
-
-  def to_h
-    JSON.parse geocode
-  end
-end
